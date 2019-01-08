a3405a233ebb75b28ed1fc2be65bf10f07d59d6e
zcmdlbd <1171829755@qq.com>
2019-01-08 11:54:56 +0800

    19.1.7

---
+++ src/xuexi/Sums.java
@@ -0,0 +1,29 @@
+package xuexi;
+
+import java.util.Arrays;
+
+public class Sums {
+	public static void main(String[] args) {
+		int[] nums = { 2, 7, 11, 15 };
+		int target = 9;
+		Solution solution = new Solution();
+
+		System.out.println(Arrays.toString(solution.twoSum(nums, target)));
+	}
+
+	static class Solution {
+		public int[] twoSum(int[] nums, int target) {
+			for (int i = 0; i < nums.length; i++) {
+				for (int j = 0; j < nums.length; j++) {
+					if (nums[i] + nums[j] == target && i != j) {
+						int[] nums1 = { i, j };
+						return nums1;
+					}
+				}
+			}
+			return nums;
+
+		}
+
+	}
+}
